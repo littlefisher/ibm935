79c39f0037d80c4c8e9e3959168e5472fd3765ad
littlefisher <ericwong@zju.edu.cn>
2018-11-11 09:14:29 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = "0.1.0"
+description = (
+    "Supports encoding conversion between `unicode` and `ibm935`",
+)
+
+long_description = open("README.md", encoding="utf8").read()
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Programming Language :: Python",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+setup(
+    name="ibm935",
+    version=version,
+    description=description,
+    long_description=long_description,
+    license="MIT",
+    author="Eric Wong",
+    author_email="ericwong@zju.edu.cn",
+    url="https://github.com/littlefisher/ibm935",
+    classifiers=classifiers,
+    py_modules=['ibm935'],
+)
